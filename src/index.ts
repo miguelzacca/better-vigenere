1e0093f9fc5c95f75d456174c9f2cb605ec98671
Miguel Zacca <miguel_zacca@estudante.sesisenai.org.br>
2024-12-22 01:36:00 -0300

    v4.0.0

--- src/index.ts
+++ src/index.ts
@@ -1,111 +1,131 @@
-type ByteSize = 8 | 16 | 32 | 64 | 128 | 256
+type IVLength = 8 | 16 | 32 | 64 | 128 | 256
 
-interface IVigenere {
+interface VigenereProperties {
   generateKey(length: number): Buffer
   encrypt(plainText: Buffer, key: Buffer): Buffer
   decrypt(cipherText: Buffer, key: Buffer): Buffer
 }
 
-interface ProcessBytes {
+interface ProcessEncrypt {
   input: Buffer
   key: Buffer
   iv?: Buffer
-  encrypt?: boolean
 }
 
-interface VigenereOptions {
-  ivLength?: ByteSize
+interface ProcessDecrypt {
+  input: Buffer
+  key: Buffer
+  iv: Buffer
 }
 
-export class Vigenere implements IVigenere {
-  static #BYTE_RANGE = 65536
-  #IV_LENGTH: ByteSize
+export interface VigenereOptions {
+  ivLength?: IVLength
+}
 
-  static get ENCODING(): 'utf16le' {
-    return 'utf16le'
-  }
+export class Vigenere implements VigenereProperties {
+  static #I32_RANGE = 4294967296
+  static #I8_RANGE = 256
+  #IV_LENGTH: IVLength
 
   constructor(options?: VigenereOptions) {
     this.#IV_LENGTH = options?.ivLength || 16
   }
 
-  #adjustLength<T extends Buffer | number>(data: T): T {
+  #adjustLength<T extends Buffer | number>(data: T): [T, number] {
+    const getPadding = (value: number) => {
+      const overflow = 4 - (value % 4)
+      return overflow === 4 ? 0 : overflow
+    }
+
     if (typeof data === 'number') {
-      return (data & 1 ? data + 1 : data) as T
+      const padding = getPadding(data)
+      const newData = (data + padding) as T
+      return [newData, padding]
     }
 
-    if (data.length & 1) {
-      return Buffer.concat([data, Buffer.alloc(1, 0x20)]) as T
+    const padding = getPadding(data.length)
+    if (padding) {
+      const newData = Buffer.concat([data, Buffer.alloc(padding, 0x20)]) as T
+      return [newData, padding]
     }
 
-    return data
+    return [data, 0]
   }
 
-  #utf8To16le(buffer: Buffer): Buffer {
-    return Buffer.from(buffer.toString(), Vigenere.ENCODING)
-  }
+  #processEncrypt({ input, key, iv }: ProcessEncrypt): Buffer {
+    const output = Buffer.allocUnsafe(input.length * 4)
 
-  #processBytes({ input, key, iv, encrypt }: ProcessBytes): Buffer {
-    const output = Buffer.allocUnsafe(input.length)
-
-    const keyLength = key.length / 2
-    const ivLength = iv ? iv.length / 2 : 0
+    const keyLength = key.length
+    const ivLength = iv ? iv.length / 4 : 0
     const ivIsDefined = iv !== undefined
 
-    for (let i = 0; i < input.length / 2; i++) {
-      const offset = i * 2
+    for (let i = 0; i < input.length; i++) {
+      const inputByte = input.readUint8(i)
+      const keyByte = key[i % keyLength]
+      const ivByte = ivIsDefined ? iv.readUint32LE((i & (ivLength - 1)) * 4) : 0
 
-      const inputByte = input.readUint16LE(offset)
-      const keyByte = key.readUint16LE((i % keyLength) * 2)
-      const ivByte = ivIsDefined ? iv.readUint16LE((i & (ivLength - 1)) * 2) : 0
+      const newByte = (inputByte + keyByte + ivByte) % Vigenere.#I32_RANGE
+      const normalized = (newByte + Vigenere.#I32_RANGE) % Vigenere.#I32_RANGE
 
-      const calc = encrypt ? inputByte + keyByte : inputByte - keyByte
-      const increment = encrypt ? ivByte : Vigenere.#BYTE_RANGE - ivByte
+      output.writeUint32LE(normalized, i * 4)
+    }
 
-      const newByte = calc + increment
-      const normalized = (newByte + Vigenere.#BYTE_RANGE) % Vigenere.#BYTE_RANGE
+    return output
+  }
 
-      output.writeUint16LE(normalized, offset)
+  #processDecrypt({ input, key, iv }: ProcessDecrypt): Buffer {
+    const output = Buffer.allocUnsafe(input.length / 4)
+
+    const keyLength = key.length
+    const ivLength = iv.length / 4
+
+    for (let i = 0; i < output.length; i++) {
+      const inputByte = input.readUint32LE(i * 4)
+      const keyByte = key[i % keyLength]
+      const ivByte = iv.readUint32LE((i & (ivLength - 1)) * 4)
+
+      const newByte = (inputByte - keyByte - ivByte) % Vigenere.#I8_RANGE
+      const normalized = (newByte + Vigenere.#I8_RANGE) % Vigenere.#I8_RANGE
+
+      output[i] = normalized
     }
 
     return output
   }
 
   #derivedKey(key: Buffer, iv: Buffer): Buffer {
-    return this.#processBytes({ input: key, key: iv, encrypt: true })
+    return this.#processEncrypt({ input: key, key: iv })
   }
 
   generateKey(length: number): Buffer {
-    const adjustedLength = this.#adjustLength(length)
-    const key = Buffer.allocUnsafe(adjustedLength)
+    const key = Buffer.allocUnsafe(length)
 
-    for (let i = 0; i < adjustedLength / 2; i++) {
-      key.writeUint16LE((Math.random() * Vigenere.#BYTE_RANGE) | 0, i * 2)
+    for (let i = 0; i < length; i++) {
+      key[i] = (Math.random() * Vigenere.#I8_RANGE) | 0
     }
 
     return key
   }
 
   encrypt(plainText: Buffer, key: Buffer): Buffer {
-    const adjustedTextLength = this.#adjustLength(plainText)
-    const text = this.#utf8To16le(adjustedTextLength)
+    const [text, padding] = this.#adjustLength(plainText)
     const iv = this.generateKey(this.#IV_LENGTH)
     const derivedKey = this.#derivedKey(key, iv)
 
-    const output = this.#processBytes({
+    const output = this.#processEncrypt({
       input: text,
       key: derivedKey,
       iv,
-      encrypt: true,
     })
 
-    return Buffer.concat([iv, output])
+    return Buffer.concat([Buffer.alloc(1, padding), iv, output])
   }
 
   decrypt(cipherText: Buffer, key: Buffer): Buffer {
-    const iv = cipherText.subarray(0, this.#IV_LENGTH)
-    const input = cipherText.subarray(this.#IV_LENGTH)
+    const iv = cipherText.subarray(1, this.#IV_LENGTH + 1)
+    const input = cipherText.subarray(this.#IV_LENGTH + 1)
     const derivedKey = this.#derivedKey(key, iv)
-    return this.#processBytes({ input, key: derivedKey, iv })
+    const output = this.#processDecrypt({ input, key: derivedKey, iv })
+    return output.subarray(0, -cipherText[0] || output.length)
   }
 }
